a3aabb6d0720acbde5ae7b6cba834d578a46fa65
Marc kender Romel Jean Charles <makender103@gmail.com>
2023-01-03 22:25:26 -0300

    fix: node-sass error by replacing it by sass

---
+++ build/precache-manifest.024ab07a3b569ef76ccde527495354c4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ccc3b9399ae252141f8",
+    "url": "/static/css/main.bca83f90.chunk.css"
+  },
+  {
+    "revision": "5ccc3b9399ae252141f8",
+    "url": "/static/js/main.06de39f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43e27cc494a1a4d88763",
+    "url": "/static/css/2.fead1b3e.chunk.css"
+  },
+  {
+    "revision": "43e27cc494a1a4d88763",
+    "url": "/static/js/2.5b7daa9f.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "42987fad9c556132d419c181f3a58b0a",
+    "url": "/static/media/visa.42987fad.svg"
+  },
+  {
+    "revision": "eee3d45a37fc601e154675a1fdc7bc19",
+    "url": "/static/media/mastercard.eee3d45a.svg"
+  },
+  {
+    "revision": "1c3e402a40a908906ad904981a33d154",
+    "url": "/static/media/visacheckout.1c3e402a.svg"
+  },
+  {
+    "revision": "af28bac1c2033bc9fbf5ac2e8c45cdb8",
+    "url": "/static/media/paypal.af28bac1.svg"
+  },
+  {
+    "revision": "1af40c0fb6ba665841dadc70f4777aea",
+    "url": "/static/media/elo.1af40c0f.svg"
+  },
+  {
+    "revision": "33a3b0132b99e99adbf4c71989284ea4",
+    "url": "/static/media/boleto.33a3b013.svg"
+  },
+  {
+    "revision": "84e926079ae1267adbb70538c9f6876f",
+    "url": "/static/media/slide.84e92607.jpg"
+  },
+  {
+    "revision": "9657566d846d0739e3f828ff889977e9",
+    "url": "/static/media/slide1.9657566d.jpg"
+  },
+  {
+    "revision": "baccf69c7d793079bcb77ac53e5350c9",
+    "url": "/static/media/slide5.baccf69c.png"
+  },
+  {
+    "revision": "9beafdd8b14b1d5147554abc7cd9844b",
+    "url": "/static/media/loginBack.9beafdd8.png"
+  },
+  {
+    "revision": "083e1f924c4a592b4ae83fdbb7a4a804",
+    "url": "/static/media/loader.083e1f92.gif"
+  },
+  {
+    "revision": "b81b436a59a800bef5e342a564c23630",
+    "url": "/static/media/vetman.b81b436a.png"
+  },
+  {
+    "revision": "5b88efb29e8a739ef1c01a03475a56cc",
+    "url": "/static/media/enfomatik.5b88efb2.png"
+  },
+  {
+    "revision": "17ac3528ffc5c52a197c224a871ae3f4",
+    "url": "/index.html"
+  }
+];
